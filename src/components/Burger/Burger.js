8c5af5d3d5213fe33a8297dea3e951fe641423d6
Nuwan <nuwan.wickramasingha@cerexio.com>
2021-01-03 23:00:24 +0530

    8. A Real App The Burger Builder (Basic Version):14. Calculating the Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,18 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const  burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return[...Array(props.ingredients[igKey])].map((_,i) => {
                return <BurgerIngredient key={igKey+i} type={igKey}/>;
             });
-        });
-    // console.log(transformedIngredients);
+        })
+        .reduce((arr,el)=>{
+            return arr.concat(el)
+        },[])
+    console.log(transformedIngredients);
+    if(transformedIngredients.length === 0){
+        transformedIngredients= <p>Please start adding ingredients!</p>    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
