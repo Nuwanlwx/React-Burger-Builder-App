cfe4e6ccfac4f530177ca0f3e0ad46944349a193
Nuwan <nuwan.wickramasingha@cerexio.com>
2021-01-14 22:08:26 +0530

    8. A Real App The Burger Builder (Basic Version):18. Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,8 +12,9 @@ const  buildControls = (props) => (
         {controls.map(ctrl => (
             <BuildControl key={ctrl.label}
                           label={ctrl.label}
-
-            added={() => props.ingredientAdded(ctrl.type)}
+                          removed = { ()=> props.ingredientRemoved(ctrl.type)}
+                          added={() => props.ingredientAdded(ctrl.type)}
+                          disabled={props.disabled[ctrl.type]}
             />
         ))}
     </div>
