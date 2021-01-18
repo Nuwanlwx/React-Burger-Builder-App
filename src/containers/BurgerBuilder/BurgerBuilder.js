48092aefcb61bf2a33e95109bd7753c1e8b7179a
Nuwan <nuwan.wickramasingha@cerexio.com>
2021-01-18 23:46:44 +0530

    8. A Real App The Burger Builder (Basic Version):25. Implementing the Button Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,7 +81,10 @@ class BurgerBuilder extends  Component {
     }
     purchaseCancelHandler = () => {
         this.setState({purchasing:false});
-        console.log(this.state.purchasing)
+        // console.log(this.state.purchasing)
+    }
+    purchaseContinueHandler = () => {
+        alert('You continue!');
     }
     render() {
         const  disabledInfo = {
@@ -93,7 +96,11 @@ class BurgerBuilder extends  Component {
        return(
            <Auxiliary>
                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                   <OrderSummary ingredients={this.state.ingredients}/>
+                   <OrderSummary
+                       ingredients={this.state.ingredients}
+                       purchaseCancelled={this.purchaseCancelHandler}
+                       purchaseContinued={this.purchaseContinueHandler}
+                   />
                </Modal>
                <Burger ingredients={this.state.ingredients}/>
                <BuildControls
