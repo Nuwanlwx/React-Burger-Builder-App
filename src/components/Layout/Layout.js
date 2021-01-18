e16b059c7152ca52131ac60076d74a1668d06500
Nuwan <nuwan.wickramasingha@cerexio.com>
2021-01-19 00:14:25 +0530

    8. A Real App The Burger Builder (Basic Version):27. Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Auxiliary from "../../hoc/Auxiliary";
 import  classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => (
     <Auxiliary>
+        <Toolbar/>
         <div>Toolbar,sideDrawer,Backdrop</div>
         <main className={classes.Content}>
             {props.children}
